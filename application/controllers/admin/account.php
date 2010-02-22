e5017626dea875d61f975897b42f673b68fbb2f1
plusjade <plusjade@gmail.com>
2010-02-22 02:33:13 -0800

    added easy account saving via email form with auto password gesent to email and change_password functionality

--- application/controllers/admin/account.php
+++ application/controllers/admin/account.php
@@ -15,15 +15,9 @@
  * manage customers.
  * this is the public wrapper.
  */
-  public function index($action=FALSE)
+  public function index()
   {  
     $content = new View('admin/account_wrapper');
-
-    # carry out the action so view is up-to-date.
-    if($action)
-      $content->response = alerts::display($this->$action());
-  
-    $content->owner = $this->owner;
 
     if(request::is_ajax())
       die($content);
@@ -33,5 +27,76 @@
     die($this->shell);
   }
 
+/*
+ * save a guest account 
+ */
+  public function save()
+  {
+    if(!$_POST)
+      die;
+    
+    $this->rsp = Response::instance();
+
+    if(!valid::email($_POST['email']))
+    {
+      $this->rsp->msg = 'Invalid Email!';
+      $this->rsp->send(); 
+    }
+    
+    $pw = text::random('alnum', 8);
+    $this->owner->email = $_POST['email'];
+    $this->owner->password = $pw;
+    $this->owner->save();
+
+    $replyto = 'unknown';
+    $body =
+      "Hi there, thanks for saving your progess over at http://pluspanda.com \r\n"
+      ."Your auto-generated password is: $pw \r\n"
+      ."You should change this password as soon as you get a chance!\r\n\n"
+      ."Thank you! - Jade from pluspanda";
+    
+    # to do FIX THE HEADERS.
+    $subject = 'Your Pluspanda account information =)';      
+    $headers = "From: welcome@pluspanda.com \r\n" .
+      "Reply-To: Jade \r\n" .
+      'X-Mailer: PHP/' . phpversion();
+      
+    mail($_POST['email'], $subject, $body, $headers);
+
+
+    $this->rsp->status = 'success';
+    $this->rsp->msg = 'Thanks, Account Saved!';
+    $this->rsp->send(); 
+  }
+  
+  
+  
+  public function change_password()
+  {
+    if(!$_POST)
+      die;
+  
+      $old_pw  = $_POST['old_pw'];
+      $salt    = $this->auth->find_salt($this->owner->password);
+      $old_pw  = $this->auth->hash_password($old_pw, $salt);
+
+      $this->rsp = Response::instance();
+      
+      if($old_pw == $this->owner->password)
+      {
+        $this->owner->password = $_POST['new_pw'];
+        $this->owner->save();
+        
+        $this->rsp->status = 'success';
+        $this->rsp->msg = 'Password Changed!';
+        $this->rsp->send(); 
+      }
+      
+      $this->rsp->msg = 'Invalid Password!';
+      $this->rsp->send(); 
+  }
+  
+  
+  
   
 } // End account Controller
--- application/views/admin/account_wrapper.php
+++ application/views/admin/account_wrapper.php
@@ -1,7 +1,27 @@
 
 <h2>Manage Account</h2>
 
-Your Username: <?php echo $owner->username?>
+Your email: <?php echo $this->owner->email?>
 
 <br/><br/>
-Your email: <?php echo $owner->email?>
+
+<h3>Change Password</h3>
+ 
+<form action="/admin/account/change_password" class="common-ajax" method="POST">
+  <fieldset>
+    <label>Old Password</label> <input type="password" name="old_pw" />
+  </fieldset>
+
+  <fieldset>
+    <label>New Password</label> <input type="text" name="new_pw" rel="length_req" alt="6"/>
+  </fieldset>
+  
+  <fieldset>
+    <label>Confirm Password</label> <input type="text" name="pw_2" rel="length_req" alt="6"/>
+  </fieldset>
+  
+  <fieldset>
+    <button type="submit">Submit</button>
+  </fieldset>
+</form>
+
