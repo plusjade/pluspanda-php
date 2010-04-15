67d3e7006308f93bd7b3e644cb0d27d4c5803ded
plusjade <plusjade@gmail.com>
2010-04-15 04:14:58 -0700

    send emails out when new testimonials are created via public form

--- application/controllers/testimonials/add.php
+++ application/controllers/testimonials/add.php
@@ -59,6 +59,29 @@ class Add_Controller extends Testimonials_Template_Controller {
         $new->meta = $_POST['meta'];
       $new->save();
       
+      
+      # send new testimonial notification email to owner.
+      if(!empty($this->owner->email))
+      {
+        $body =
+          "Hi there, someone created a testimonial using your public pluspanda form.\r\n"
+          ."To view and edit it, log in at: http://pluspanda.com/admin/login \r\n"
+          ."\r\n"
+          ."The name of the submitter is: {$new->name}\r\n"
+          ."The submitter is likely editing their testimonial right now so give them some time to finish =)\r\n"
+          ."Have questions or need help just email plusjade@gmail.com - Thanks!"
+        ;
+        
+        # to do FIX THE HEADERS.
+        $subject = 'you received a new testimonial via pluspanda...';      
+        $headers = "From: pluspanda@pluspanda.com \r\n" .
+          "Reply-To: Jade \r\n" .
+          'X-Mailer: PHP/' . phpversion();
+          
+        mail($this->owner->email, $subject, $body, $headers);
+      }
+      
+      
       $editor_url = url::site("testimonials/save/{$this->owner->apikey}?ttk={$new->token}");
       url::redirect($editor_url);
     }
